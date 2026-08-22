aea2668556a56b66e9cebeb05ab61d0506c420dd
SandeepKasetti <kasettisandeep28@gmail.com>
2026-08-22 17:14:38 +0530

    Add files via upload

---
+++ frontend/contract-config.js
@@ -0,0 +1,18 @@
+export const CONTRACT_ADDRESS =
+    "0xe9cA989dDa5d6Ebf3bAE29CA470ACad0768751e5";
+
+export const MONAD_TESTNET = {
+    chainId: "0x279f",
+    chainName: "Monad Testnet",
+    nativeCurrency: {
+        name: "MON",
+        symbol: "MON",
+        decimals: 18
+    },
+    rpcUrls: [
+        "https://testnet-rpc.monad.xyz"
+    ],
+    blockExplorerUrls: [
+        "https://testnet.monadscan.com"
+    ]
+};
